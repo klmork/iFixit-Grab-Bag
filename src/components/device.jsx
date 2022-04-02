d6d196029d5032575120781391629fe7d6347f78
klmork <klmork@calpoly.edu>
2022-04-01 23:33:24 -0700

    Change number of columns based on window size.

--- src/components/device.jsx
+++ src/components/device.jsx
@@ -11,12 +11,13 @@ class Device extends React.Component {
             <div className="Device">
                 {/*<span>Device { device.display_title } </span><br></br>*/}
                 
-                {/* Pick smaller image when screen size is smaller 
-                ... 780 fits thumbnail nicely*/}
+                {/* Pick smaller image when screen size is smaller:
+                    -> since columns are already reduced as screen scales down,
+                       shouldn't switch to thumbnail until screen is around 320px (maybe phones) */}
                 {/*TODO: maybe organize css and resizing better*/}
                 <img
                     srcSet= {srcImage}
-                    sizes = "(max-width: 780px) 96px, 300px"
+                    sizes = "(max-width: 300px) 96px, 320px"
                     src = {device.image.standard} 
                     alt="device"
                 />
