d1a3f1ca94e90996534362465373cf838f30e8cb
klmork <klmork@calpoly.edu>
2022-04-01 19:27:54 -0700

    Added pagination.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,98 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-const API_URL = "https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=0&limit=10";
+const API_URL = "https://www.ifixit.com/api/2.0/wikis/CATEGORY?";
 
 class App extends React.Component {
   
   state = {
     devices: [],
+    page: 0,
+    numDevicesDisplayed: 10
   };
 
   componentDidMount() {
     this.handleFetch();
   }
 
-  // fetch data from API - for now just first 10 pages
-  handleFetch() {
-    fetch(API_URL)
+  // ----------- Event Handlers --------------------------------------
+
+  // Fetch data from API (only for devices currently being displayed)
+  handleFetch = () => {
+    const { page, numDevicesDisplayed } = this.state;
+    const offset = "offset=" + page * numDevicesDisplayed;
+    const limit = "&limit=" + numDevicesDisplayed;
+
+    fetch(API_URL+offset+limit)
         .then((res) => res.json())
         .then((json) => {
             this.setState({
               devices: json 
             });
         })
+  };
+
+  // Pagination - page left and load devices
+  handleDecrement = () => {
+      if (this.state.page === 0) return;
+
+      const page = this.state.page - 1;
+      this.setState({ page }, this.handleFetch);
+  };
+
+  // Pagination - page right and load devices
+  handleIncrement = () => {
+    const page = this.state.page + 1;
+    this.setState({ page }, this.handleFetch);
+
+  };
+    
+  // ------------------------ Helper Methods -------------------
+
+  getBackBtnClasses() {
+    let classes = "btn m-2 b-4 btn-";
+    classes += this.state.page === 0 ? "basic" : "primary";
+    return classes;
   }
 
+
+  // -------------------- Render ----------------------------
   render() { 
     const { devices } = this.state;
 
-    //TODO: make grid ... testing as list for now
+    //TODO: make grid and other components... testing as list for now
     return (
       <div className = "App">
         <h1>Device Grab Bag</h1>
         <div>
           {
             devices.map(device => (
-              <ol key = { device.id }>
+              <ol key={ device.wikiid }>
                 <span>Device { device.display_title } </span><br></br>
-                <img src={device.image.thumbnail} alt="image of device"/>
+                <img src={device.image.thumbnail} alt="device"/>
               </ol>
             ))
           }
         </div>
+        <p>page {this.state.page}</p>
+        <button 
+          className={this.getBackBtnClasses()} 
+          onClick={this.handleDecrement}
+        >
+          Back
+        </button>
+        <button 
+          className="btn btn-primary m-2 b-4" 
+          onClick={this.handleIncrement}
+        >
+          Next
+        </button>
       </div>
       
     );
   }
+
+
 }
  
 export default App;
