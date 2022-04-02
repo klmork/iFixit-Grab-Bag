741096442e1400c4e82ee5e15294826ea2599715
klmork <klmork@calpoly.edu>
2022-04-01 18:41:48 -0700

    Tested retrieving images from database.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_URL = "https://www.ifixit.com/api/2.0/wikis/CATEGORY?offset=0&limit=10";
 
+class App extends React.Component {
+  
+  state = {
+    devices: [],
+  };
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  // fetch data from API - for now just first 10 pages
+  handleFetch() {
+    fetch(API_URL)
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+              devices: json 
+            });
+        })
+  }
+
+  render() { 
+    const { devices } = this.state;
+
+    //TODO: make grid ... testing as list for now
+    return (
+      <div className = "App">
+        <h1>Device Grab Bag</h1>
+        <div>
+          {
+            devices.map(device => (
+              <ol key = { device.id }>
+                <span>Device { device.display_title } </span><br></br>
+                <img src={device.image.thumbnail} alt="image of device"/>
+              </ol>
+            ))
+          }
+        </div>
+      </div>
+      
+    );
+  }
+}
+ 
 export default App;
